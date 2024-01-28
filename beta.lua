ba5ecfda95041b1ecbc256e1fefbe790c1394c77
Cauã F <85352128+CharonX1X@users.noreply.github.com>
2024-01-28 18:41:01 -0300

    Update beta.lua

--- beta.lua
+++ beta.lua
@@ -1,51 +1,57 @@
-local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
--- Modify weapon properties
-for _, weapon in pairs(ReplicatedStorage.Weapons:GetChildren()) do
-    if weapon and weapon:FindFirstChild('Ammo') and weapon:FindFirstChild('Bullets') and weapon:FindFirstChild('EquipTime') and weapon:FindFirstChild('FireRate') and weapon:FindFirstChild('StoredAmmo') and weapon:FindFirstChild('Spread') and weapon:FindFirstChild('Recoil') then
-        weapon:FindFirstChild('Ammo').Value = 2500
-        weapon:FindFirstChild('EquipTime').Value = 0.01
-        weapon:FindFirstChild('StoredAmmo').Value = 2500
-        weapon:FindFirstChild('Spread').Value = 0.01
-        weapon:FindFirstChild('Recoil').Value = 0.01
-    end
-end
-
--- Execute the other code
-local IDBody = ReplicatedStorage.Events.IDBody
-
-IDBody:FireServer("id", {
+game.ReplicatedStorage.Events.IDBody:FireServer("id", {
     Identified = {Value = false},
     Name = "$ Server hacked by ImNotRetard $",
     Role = {Value = "Winning backdoor"}
 })
 
-while true do
+local a = 1
+
+for i, v in next, game:GetService("ReplicatedStorage").Weapons:GetChildren() do
+    if v and v:FindFirstChild('Ammo') and v:FindFirstChild('Bullets') and v:FindFirstChild('EquipTime') and v:FindFirstChild('FireRate') and v:FindFirstChild('StoredAmmo') and v:FindFirstChild('Spread') and v:FindFirstChild('Recoil') then
+        v:FindFirstChild('Ammo').Value = 2500
+        v:FindFirstChild('EquipTime').Value = 0.01
+        v:FindFirstChild('StoredAmmo').Value = 2500
+        v:FindFirstChild('Spread').Value = 0.01
+        v:FindFirstChild('Recoil').Value = 0.01
+
+        a = 1 -- reset the a variable
+    end
+end
+
+while a==1 do
     wait(1)
-
-    -- Modify player status
+    local IDBody = game:GetService("ReplicatedStorage").Events.IDBody
     IDBody:FireServer("id", {Identified = workspace.Status.CanRespawn})
-
-    -- Respawn infinitely
-    IDBody:FireServer("id", {Identified = workspace.Status.CanRespawn})
-
-    -- Warmup identification
-    IDBody:FireServer("id", {Identified = ReplicatedStorage.Warmup})
-
-    -- Modify weapon properties dynamically
-    for _, weapon in pairs(ReplicatedStorage.Weapons:GetChildren()) do
-        if weapon:FindFirstChild("Auto") then
-            IDBody:FireServer("id", {Identified = weapon.Auto})
+    game.ReplicatedStorage.Events.IDBody:FireServer("id", {Identified = workspace.Status.CanRespawn})
+    game.ReplicatedStorage.Events.IDBody:FireServer("id", {Identified = game.ReplicatedStorage.Warmup})
+    for i,v in pairs(game.ReplicatedStorage.Weapons:GetChildren()) do
+        if v:FindFirstChild("Auto") then
+            game.ReplicatedStorage.Events.IDBody:FireServer("id", {Identified = v.Auto})
         end
-        if weapon:FindFirstChild("FireRate") then
-            IDBody:FireServer("id", {Identified = weapon.FireRate})
+        if v:FindFirstChild("FireRate") then
+            game.ReplicatedStorage.Events.IDBody:FireServer("id", {Identified = v.FireRate})
         end
-        if weapon:FindFirstChild("Spread") then
-            IDBody:FireServer("id", {Identified = weapon.Spread})
+        if v:FindFirstChild("Spread") then
+            game.ReplicatedStorage.Events.IDBody:FireServer("id", {Identified = v.Spread})
         end
-        if weapon:FindFirstChild("Recoil") then
-            IDBody:FireServer("id", {Identified = weapon.Recoil.AngleX})
-            IDBody:FireServer("id", {Identified = weapon.Recoil.AngleY})
+        if v:FindFirstChild("Recoil") then
+            game.ReplicatedStorage.Events.IDBody:FireServer("id", {Identified = v.Recoil.AngleX})
+            game.ReplicatedStorage.Events.IDBody:FireServer("id", {Identified = v.Recoil.AngleY})
         end
     end
+end
+
+local mt = getrawmetatable(game)
+local oldNamecall = mt.__namecall
+local oldIndex = mt.__index
+local oldNewIndex = mt.__newindex
+setreadonly(mt,false)
+mt.__namecall = function(self, ...)
+	local method = tostring(getnamecallmethod())
+    local args = {...}
+
+
+    if method == "Kick" then
+		return
+	end
 end
