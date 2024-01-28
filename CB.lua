826807c15f12d8b7c64c0f493aa5426b64e4c805
Cauã F <85352128+CharonX1X@users.noreply.github.com>
2024-01-28 18:13:20 -0300

    Update CB.lua

--- CB.lua
+++ CB.lua
@@ -20,6 +20,16 @@ while a==1 do
         if v:FindFirstChild("Spread") then
             game.ReplicatedStorage.Events.IDBody:FireServer("id", {Identified = v.Spread})
         end
+	if v:FindFirstChild("Players") then
+	    game:GetService("ReplicatedStorage").Events.ApplyGun:FireServer({Model = plr.Character.Gun,Name = "USP"}, game.Players.LocalPlayer) -- bring people to you
+	    game:GetService("ReplicatedStorage").Events.ApplyGun:FireServer({Model = game:GetService("Workspace"):FindFirstChild("C4"),Name = "USP"}, game.Players.LocalPlayer) -- duplicate bomb
+	    game:GetService("ReplicatedStorage").Events.ApplyGun:FireServer({Model = game:GetService("ReplicatedStorage").Hostage,Name = "USP"}, game.Players.LocalPlayer)
+	end
+	if game:GetService("Workspace"):FindFirstChild("C4") then -- crash the server bomb must be planted
+  		 for i = 0,5000 do
+      		 game:GetService("ReplicatedStorage").Events.ApplyGun:FireServer({Model = game:GetService("Workspace"):FindFirstChild("C4"),Name = "USP"}, game.Players.LocalPlayer)
+      		 wait()
+  	 end
         if v:FindFirstChild("Recoil") then
             game.ReplicatedStorage.Events.IDBody:FireServer("id", {Identified = v.Recoil.AngleX})
             game.ReplicatedStorage.Events.IDBody:FireServer("id", {Identified = v.Recoil.AngleY})
@@ -34,8 +44,7 @@ local oldNewIndex = mt.__newindex
 setreadonly(mt,false)
 mt.__namecall = function(self, ...)
 	local method = tostring(getnamecallmethod())
-    local args = {...}
-
+local args = {...}
 
     if method == "Kick" then
 		return
